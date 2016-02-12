5c04fdbfcd9428049b9438f80c173d7b6040cccd
chudaeyoum <chudaeyoun@naver.com>
2016-02-12 13:09:16 +0900

    first commit

---
+++ app/src/main/java/com/example/jinwoo/edulock/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.jinwoo.edulock;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+//public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+    private Button onBtn, offBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        onBtn = (Button)findViewById(R.id.btn1);
+        offBtn = (Button)findViewById(R.id.btn2);
+
+        onBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ScreenService.class);
+                startService(intent);
+            }
+        });
+        offBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View v){
+                Intent intent = new Intent(MainActivity.this, ScreenService.class);
+                stopService(intent);
+            }
+        });
+    }
+}
