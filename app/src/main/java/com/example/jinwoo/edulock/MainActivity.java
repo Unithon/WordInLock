fc331a9d7d24fbc32f3982ca0725e213360b363b
chudaeyoum <chudaeyoun@naver.com>
2016-02-12 14:17:03 +0900

    test3

--- app/src/main/java/com/example/jinwoo/edulock/MainActivity.java
+++ app/src/main/java/com/example/jinwoo/edulock/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+       //fe
         onBtn = (Button)findViewById(R.id.btn1);
         offBtn = (Button)findViewById(R.id.btn2);
 
